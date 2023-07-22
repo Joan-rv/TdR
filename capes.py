0ff08fc96fb821f2b6d24d01b26cb5190aac10e4
Joan Ripoll <Joan-rv@users.noreply.github.com>
2023-07-22 13:10:01 +0200

    Afegir inicialització de paràmetres Xavier

--- capes.py
+++ capes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import optimitzadors
 
 class Capa():
     def __init__(self):
@@ -18,26 +19,40 @@ class Capa():
 
 
 class Perceptró(Capa):
-    def __init__(self, dimensions_entrada, dimensions_sortida):
-        self.W = np.random.randn(dimensions_sortida, dimensions_entrada)
-        self.b = np.random.randn(dimensions_sortida, 1)
+    def __init__(self, dimensions_entrada, dimensions_sortida, optimitzador='cap'):
+        # inicialització Xavier
+        maxim = (1.0 / np.sqrt(dimensions_entrada + dimensions_sortida))
+        minim = - maxim
+        self.W = minim + np.random.random((dimensions_sortida, dimensions_entrada)) * (maxim - minim)
+        self.b = minim + np.random.random((dimensions_sortida, 1)) * (maxim - minim)
+        if optimitzador == 'cap':
+            self.optimitzador = optimitzadors.Cap()
+        elif optimitzador == 'adam':
+            self.optimitzador = optimitzadors.Adam(dimensions_sortida, dimensions_entrada)
+        else:
+            raise Exception(f"Optimitzador {optimitzador} desconegut")
     
     def propaga(self, entrada):
         self.entrada = entrada
         return self.W.dot(entrada) + self.b
 
-    def retropropaga(self, delta, alfa):
+    def retropropaga(self, delta, alfa, iter):
         _, m = self.entrada.shape
 
         delta_nou = self.W.T.dot(delta)
 
+
+        #dW = 1/m * delta.dot(self.entrada.T)
+        #db = np.reshape(1/m * np.sum(delta, 1), self.b.shape)
+
+        #self.W, self.b = self.optimitzador.actualitza(alfa, self.W, dW, self.b, db, iter)
         self.W -= alfa * 1/m * delta.dot(self.entrada.T)
         self.b -= alfa * 1/m * np.reshape(1/m * np.sum(delta, 1), self.b.shape)
 
         return delta_nou
-    
+
     def __str__(self):
-        return self.__class__.__name__ + str(self.W.shape)
+        return self.__class__.__name__ + str(self.W.shape) + self.optimitzador.__class__.__name__
     
     def __repr__(self):
         return self.__str__()
