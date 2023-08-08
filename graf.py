391c67ed5f8d8ef700a8fd2e372d7f83513b9ad2
Joan Ripoll <Joan-rv@users.noreply.github.com>
2023-08-08 13:02:41 +0200

    Fer gràfica NOT utilitzar xarxa neuronal

--- graf.py
+++ graf.py
@@ -37,17 +37,34 @@ eixos(ax)
 plt.legend()
 plt.savefig('Gràfica_sig_tanh.svg', bbox_inches='tight')
 
-x = np.linspace(0, 1, 100)
+xarxa = XarxaNeuronal([
+    Perceptró(1, 1),
+])
 
-def recta_not(x):
-    # m = -1, n = + 1/2
-    return -1*x + 1/2
+X = np.array([[0], [1]]).T
+Y = np.array([[1], [-1]]).T
+
+alfa = 0.1
+iteracions = 1000
+i = 0
+precisió_entrenament = 0
+while precisió_entrenament < 0.99:
+    i += 1
+    sortida = xarxa.propaga(X)
+    xarxa.retropropaga(alfa, d_eqm, Y, i)
+    precisió_entrenament = np.sum(2*(sortida > 0) - 1 == Y)/Y.shape[1]
+    if i % 100 == 0:
+        print(f"Iteració: {i}; precisió: {precisió_entrenament*100:.2f}%")
+
+print(f"Iteració: {i}; precisió: {precisió_entrenament*100:.2f}%")
+
+
+x = np.linspace(-1, 1, 100).reshape((1, 100))
 
 fig, ax = plt.subplots()
-ax.set_ylim(-0.6, 0.6)
 ax.axhline(0, color='black', linewidth=.5)
 ax.axvline(0, color='black', linewidth=.5)
-ax.plot(x, recta_not(x))
+ax.plot(x.T, xarxa.propaga(x).T)
 
 eixos(ax)
 
@@ -59,31 +76,6 @@ ax.set_ylabel("Sortida")
 
 plt.savefig('Gràfica_recta_not.svg', bbox_inches='tight')
 
-
-if False:
-    def pla_and(x, y):
-        return x + y - 3/2
-
-    x = np.linspace(0, 1, 101)
-    y = np.linspace(0, 1, 101)
-
-    X, Y = np.meshgrid(x, y)
-
-
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-
-    Z = pla_and(X, Y)
-    norm = colors.TwoSlopeNorm(vmin=-1.5, vcenter=0, vmax=0.5)
-    ax.plot_surface(X, Y, Z, cmap=colors.LinearSegmentedColormap.from_list('rg',["r", "g"], N=2), rstride=2, cstride=2, norm=norm)
-    Z = np.zeros_like(Z)
-    ax.plot_surface(X, Y, Z, alpha=0.2)
-
-    ax.set_xlabel("$x_1$")
-    ax.set_ylabel("$x_2$")
-    ax.set_zlabel("$y$")
-
-    plt.savefig('Gràfica_pla_and.svg', bbox_inches='tight')
 
 def graficà_3d_model(model, nom_fitxer_sortida):
 
