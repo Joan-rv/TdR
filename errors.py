c84779fdcfa06c42d49a0d633aa2fb2ead670e93
Joan Ripoll <Joan-rv@users.noreply.github.com>
2023-08-24 13:02:17 +0200

    Afegir més funcions d'error

--- errors.py
+++ errors.py
@@ -1,21 +1,28 @@
 import numpy as np
 
-# error quadràtic mig
-
-
 def eqm(y, ŷ):
     return np.sum(np.square(ŷ - y)) / y.size
-
-# derivada
-
 
 def d_eqm(y, ŷ):
     return 2*(ŷ - y)
 
 
+def eam(y, ŷ):
+    return np.sum(np.absolute(ŷ - y)) / y.size
+
+def d_eam(y, ŷ):
+    return (ŷ > y) * 2 - 1
+
+
+def eqlm(y, ŷ):
+    return np.sum(np.square(np.log(1 + ŷ) - np.log(1 + y))) / y.size
+
+def d_eqlm(y, ŷ):
+    return 2*(np.log(1 + ŷ) - np.log(1 + y)) * 1 / (1 + ŷ)
+
+
 def entropia_creuada(y, ŷ):
     return -np.mean(y*np.log(ŷ) + (1 - y)*np.log(1 - ŷ))
 
-
 def d_entropia_creuada(y, ŷ):
-    return - (y/(ŷ + 1e-70) - (1 - y)/(1 - ŷ))
+    return -(y/(ŷ + 1e-70) - (1 - y)/(1 - ŷ))
